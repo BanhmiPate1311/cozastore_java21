099604e9de113d0dc2a2de5a970e82df204339a1
huynhcuong <huynhcuong@gmail.com>
2023-06-15 18:28:49 +0700

    api login and signup

---
+++ src/main/java/com/cybersoft/cozastore_java21/filter/JwtFilter.java
@@ -0,0 +1,56 @@
+package com.cybersoft.cozastore_java21.filter;
+
+import com.cybersoft.cozastore_java21.utils.JwtHelper;
+import io.jsonwebtoken.Claims;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.ArrayList;
+
+@Component
+public class JwtFilter extends OncePerRequestFilter {
+
+    /**
+     * Nhận được token truyền trên header
+     * Giải mã token
+     * Nếu giải mã thành công thì hợp lệ
+     * Tạo chứng thực và cho đi vào link người dùng gọi
+     */
+
+    @Autowired
+    private JwtHelper jwtHelper;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        try {
+            // Lấy giá trị của header có key là Authorization
+            String header = request.getHeader("Authorization");
+            if (header.startsWith("Bearer ")) {
+                // Cắt bỏ chữ Bearer và lấy ra token
+                String token = header.substring(7);
+                Claims claims = jwtHelper.decodeToken(token);
+                if (claims != null) {
+                    // Tạo chứng thực cho Security
+                    SecurityContext context = SecurityContextHolder.getContext();
+                    UsernamePasswordAuthenticationToken user = new UsernamePasswordAuthenticationToken("","",new ArrayList<>());
+                    context.setAuthentication(user);
+                }
+            }
+        } catch (Exception e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+
+
+
+        filterChain.doFilter(request, response);
+    }
+}
